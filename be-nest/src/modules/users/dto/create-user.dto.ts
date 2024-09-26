4d26ab1f70775a9f1b262311d8bbc42523dbfce7
KhoaHocMai113 <doduongdangkhoa1103@gmail.com>
2024-09-26 13:31:20 +0700

    Create and get user

--- be-nest/src/modules/users/dto/create-user.dto.ts
+++ be-nest/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsEmpty, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Invalid email' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
+
+  phone: string;
+  address: string;
+  image: string;
+}
