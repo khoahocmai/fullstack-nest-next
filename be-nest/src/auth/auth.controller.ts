e9261bb5456f02a4cd16b4792f0ce6a00f240e50
KhoaHocMai113 <doduongdangkhoa1103@gmail.com>
2024-10-27 17:49:08 +0700

    Video 9

--- be-nest/src/auth/auth.controller.ts
+++ be-nest/src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { Public } from '@/decorator/customize';
 import {
   Body,
   Controller,
@@ -7,14 +8,16 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './passport/local-auth.guard';
-import { JwtAuthGuard } from './passport/jwt-auth.guard';
-import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { LocalAuthGuard } from './passport/local-auth.guard';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @Post('login')
   @Public()
@@ -28,5 +31,17 @@ export class AuthController {
   @Public()
   register(@Body() body: CreateAuthDto) {
     return this.authService.register(body);
+  }
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: 'doduongdangkhoa1103@gmail.com', // list of receivers
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      html: '<b>Hello word with me</b>', // HTML body content
+    });
+    return 'Ok';
   }
 }
